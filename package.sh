ed6239a2def0aa1bda15d6e8d5b8515073fc52bd
Thomas Williams <t.williams@psenterprise.com>
2020-06-11 20:35:20 +0100

    Add bash script to create a release zip

---
+++ package.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+release=1
+release_dir="zmim-r$release"
+base="$release_dir/zmim"
+
+mkdir -p $base
+mkdir -p $base/saves
+mkdir -p $base/scripts
+mkdir -p $base/src
+mkdir -p $base/stories
+
+cp LICENSE $base
+cp zmim.bas $base
+cp -R src/* $base/src
+cp stories/minizork.z3 $base/stories
+cp stories/sampler1.z3 $base/stories
+cp stories/sampler2.z3 $base/stories
+cp stories/tutorial.z3 $base/stories
+cp docs/zmim.pdf $base
+
+cd $release_dir
+zip -r ../$release_dir.zip zmim
+cd .
